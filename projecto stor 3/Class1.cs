54b87c4be1302841ecee0e26184affea2004e84c
Rui Azevedo <ruiazevedo1666@gmail.com>
2020-03-04 12:12:47 +0000

    Adicionar arquivos de projeto.

---
+++ projecto stor 3/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projecto_stor_3
+{
+    class Class1
+    {
+        public void Move(object sender)
+        {
+            Random r = new Random();
+            int _x = (int.Parse(r.Next(500).ToString()));
+            int _y = (int.Parse(r.Next(250).ToString()) + 100);
+            Point pt = new Point(_x,_y);
+            Button b = (Button)sender;
+            b.Location = pt;
+
+        }
+    }
+}
